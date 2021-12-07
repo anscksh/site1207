b6148b76db85dd32ab3c780b32fe9503bec07872
anscksh <moonchanho1203@naver.com>
2021-12-07 14:38:49 +0900

    14:40

--- js/function.js
+++ js/function.js
@@ -1,3 +1,31 @@
 function openMenu(x) {
-    x.classList.toggle("change");
+    document.getElementById("menuIcon").innerHTML = "<i class=\"fa fa-times\"></i>";
+}
+
+var acc = document.getElementsByClassName("acc");
+
+for (var i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var text = this.nextElementSibling;
+        if (text.style.display === "block") {
+            text.style.display = "none";
+        } else {
+            text.style.display = "block";
+        }
+    });
+}
+
+function openCity(event, cityName) {
+    var i;
+    var tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    var tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    event.currentTarget.className += " active";
 }
