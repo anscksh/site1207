c8247eced7ffb2410dda46933f7bbf41455b6d67
anscksh <moonchanho1203@naver.com>
2021-12-08 12:03:49 +0900

    1208_12:05

--- js/practice2.js
+++ js/practice2.js
@@ -1,10 +1,45 @@
-// 스크롤시 네비바
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
+// 스크롤 다운 시 네비바
+window.onscroll = function() {scrollDownFunction()};
+function scrollDownFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-50px";
+  }
+}
+
+//스크롤 업 시 네비바
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+//쉬링크 네비바
+window.onscroll = function() {scrollShrinkFunction()};
+function scrollShrinkFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
+
+//스티키 네비바
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
   }
 }
