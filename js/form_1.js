1b4fc8c0e44416f1814b1f5e20ab3b1be65abbe9
anscksh <moonchanho1203@naver.com>
2021-12-09 17:17:46 +0900

    1209_17:15

---
+++ js/form_1.js
@@ -0,0 +1,6 @@
+
+var modal = document.getElementById('id01');
+
+function closeModal() {
+    modal.style.display = "none";
+}
---
+++ js/form_2.js
@@ -0,0 +1,8 @@
+// 오픈 팝업
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
